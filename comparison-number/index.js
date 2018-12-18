43e0de4945cdbb1e52972b7954e41b08cfe57aea
Rina Hafizhah U <rinahafizhah@gmail.com>
2018-12-18 10:12:29 +0700

    add comparison num

---
+++ comparison-number/index.js
@@ -0,0 +1,16 @@
+var num1 = 40;
+var num2 = 20;
+if (num1 > num2) {
+  console.log("Amazing! You're right. num1 larger than num2");
+} else if (num2 > num1) {
+  console.log("It's bad. You're wrong. Num2 smaller than num1");
+} else {
+  console.log("You're doing a great job!");
+}
+
+grade = 50;
+if (grade >= 80 && grade >= 90) {
+  console.log("Brilliant student");
+} else {
+  console.log("Average student");
+}
