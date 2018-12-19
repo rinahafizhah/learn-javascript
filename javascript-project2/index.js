8521d7440ac9cc232423730aed27b88740c52d27
Rina Hafizhah U <rinahafizhah@gmail.com>
2018-12-19 11:57:25 +0700

    add level 3 + 4

--- javascript-project2/index.js
+++ javascript-project2/index.js
@@ -9,11 +9,13 @@ const products = [
 
 console.log(products);
 
+//Level 0
 for (let i = 0; i < products.length; i++) {
   document.writeln(products[i]);
   document.write("<br>");
 }
 
+//Level 1
 const identifyFood = products.filter(function(food) {
   return food.toLocaleLowerCase().includes("spicy".toLocaleLowerCase());
 });
@@ -29,10 +31,34 @@ const identifyDrink = products.filter(function(drink) {
 });
 console.log(identifyDrink);
 
+//Level 2
 const identifyMenu = products.map((menu, i) => {
   if (menu.includes("spicy")) console.log(`${i + 1}. ${menu} (food)`);
   else if (menu.includes("hot")) console.log(`${i + 1}. ${menu} (drink)`);
   else console.log(`${i + 1}. ${menu} (drink)`);
 });
 
+//Level 3
 
+const searchMenu = products.filter(m => m.includes("noodle"));
+console.log(searchMenu);
+
+//Level 4
+const menu1 = ["hot choco", "hot tea", "Cold soda"];
+const menu2 = ["cold tea", "spicy noodle", "spicy fried rice"];
+console.log(menu1.concat(menu2));
+
+//Level 5
+const menu_resto = [
+    "hot choco",
+    null,
+    undefined,
+    "hot tea",
+    10,
+    "Cold soda",
+    "cold tea",
+    7,
+    9,
+    "spicy noodle",
+    "spicy fried rice"
+]
